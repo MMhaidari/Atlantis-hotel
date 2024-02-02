944213e5fc0376d70a09d3ac0290ea6b1c1ec0ad
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-02-02 15:46:31 +0430

    styled components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import GlobalStyles from "./styles/GlobalStyles"
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
 
-function App() {
-
+const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>
-        Hello
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard'/>} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
