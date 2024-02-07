e40d782f04b5f32b035a45c01e71ecb93631d997
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-02-07 19:54:16 +0430

    cabins data added and delete functionality also added

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +39,35 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
+
+const CabinRow = ({ cabin }) => {
+  const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin
+
+  const queryClient = useQueryClient()
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['cabins']
+      })
+    },
+    onError: (error) => alert(error.message)
+  })
+
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
+    </TableRow>
+  )
+}
+
+export default CabinRow
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from '../../services/apiCabins.js';
+import Spinner from '../../ui/Spinner.jsx'
+import CabinRow from "./CabinRow.jsx";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +27,28 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+const CabinTable = () => {
+  const { data: cabins, isLoading, error } = useQuery({
+    queryKey: ['cabins'],
+    queryFn: getCabins
+  })
+
+  if (isLoading) return <Spinner />
+
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => <CabinRow cabin={cabin} key={cabin.id} />)}
+    </Table>
+  )
+}
+
+export default CabinTable
