0ef761ede440587a0320f955c15a4152bec36e4d
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-02-11 21:42:57 +0430

    Modal window for creating cabins converted to a compound component'

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,6 +1,8 @@
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -51,19 +53,48 @@ const Button = styled.button`
   }
 `;
 
+const ModalContext = createContext();
 
-const Modal = ({ children, onClose }) => {
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
-        <Button onClick={onClose}><HiXMark /></Button>
-        <div>
-          {children}
-        </div>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
+          <HiXMark />
+        </Button>
+
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
-  )
+  );
 }
 
-export default Modal
+Modal.Open = Open;
+Modal.Window = Window;
+
+export default Modal;
